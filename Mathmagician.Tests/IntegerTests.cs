5efe00cc6528e3ad39f6416744050e078565aca5
Bradley Guthrie <guppy96@gmail.com>
2016-08-01 21:19:22 -0500

    unitest project setup and first integer test

---
+++ Mathmagician.Tests/IntegerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class IntegerTests
+    {
+        [TestMethod]
+        public void IntegerEnsureICanCreateAnInstance()
+        {
+
+            //Arrange
+
+
+            //Act
+            Integer my_int = new Integer();
+
+            //Assert
+            Assert.IsNotNull(my_int);
+
+        }
+    }
+}
