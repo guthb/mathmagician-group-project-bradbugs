8517bf4952d97066468935b22f93aa1825c747f0
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-04 18:34:04 -0500

    Main test passing

--- Mathmagician.Tests/PrimesTests.cs
+++ Mathmagician.Tests/PrimesTests.cs
@@ -27,5 +27,16 @@ namespace Mathmagician.Tests
             //Assert
             Assert.AreEqual(my_prime.ReturnPrimeNumbersList(0).GetType(), my_list.GetType());
         }
+
+        [TestMethod]
+        public void PrimesListPrimesOutputs13EarlyPrimes()
+        {
+            //Arrange
+            Primes my_prime = new Primes();
+            List<int> my_list = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41 };
+            //Act
+            //Assert
+            CollectionAssert.AreEqual(my_list, my_prime.ReturnPrimeNumbersList(13));
+        }
     }
 }
