4bc44b6e1136592fa097b34e3a3e112b0c140cee
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-04 17:43:53 -0500

    Primes scaffold

--- Mathmagician/Primes.cs
+++ Mathmagician/Primes.cs
@@ -6,55 +6,13 @@ using System.Threading.Tasks;
 
 namespace Mathmagician
 {
-    public class Primes
+    class Primes
     {
-        public static bool IsPrime(int candidate)
+
+        public List<int> ReturnPrimeNumbersList(int userInput)
         {
-            // Test whether the parameter is a prime number.
-            if ((candidate & 1) == 0)
-            {
-                if (candidate == 2)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            for (int i = 3; (i * i) <= candidate; i += 2)
-            {
-                if ((candidate % i) == 0)
-                {
-                    return false;
-                }
-            }
-            return candidate != 1;
+            throw new NotImplementedException();
         }
 
-        public List<int> ListPrimes(int iterations)
-        {
-            List<int> primes = new List<int>();
-            int i = 0;
-            int j = 0;
-
-            while(i < iterations)
-            {
-                bool prime = IsPrime(j);
-
-                if (prime)
-                {
-                    primes.Add(j);
-                    i++;
-                    j++;
-                }
-                else
-                {
-                    j++;
-                }
-            }
-
-            return primes;
-        }
     }
 }
