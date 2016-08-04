ea84dcd6d892b342eb9976f69664f8cfab25d360
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-04 17:45:54 -0500

    PrimesTest scaffold

--- Mathmagician.Tests/PrimesTests.cs
+++ Mathmagician.Tests/PrimesTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 
 namespace Mathmagician.Tests
 {
@@ -8,41 +7,8 @@ namespace Mathmagician.Tests
     public class PrimesTests
     {
         [TestMethod]
-        public void PrimesEnsureICanCreateAnInstance()
+        public void ()
         {
-            //Arrange
-            Primes myPrime = new Primes();
-
-            //Act
-
-            //Assert
-            Assert.IsNotNull(myPrime);
-        }
-
-        [TestMethod]
-        public void PrimesListPrimesMethodReturnsAList()
-        {
-            //Arrange
-            Primes myPrime = new Primes();
-
-            //Act
-
-            //Assert
-            Assert.IsInstanceOfType(myPrime.ListPrimes(2), typeof(List<int>));
-        }
-
-        [TestMethod]
-        public void PrimesListPrimesMethodCorrelatesToEarlyPrimes()
-        {
-            //Arrange
-            List<int> testOfEarlyPrimes = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19 };
-
-            //Act
-            Primes myPrime = new Primes();
-
-            //Assere
-            CollectionAssert.AreEqual(testOfEarlyPrimes, myPrime.ListPrimes(8));
-
         }
     }
 }
