0feacf8d1a42a28609f10ae3937ba178bc82cb56
Bradley Guthrie <guppy96@gmail.com>
2016-08-04 19:47:45 -0500

    odds class test

---
+++ Mathmagician/Odds.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Mathmagician
+{
+    public class Odds
+    {
+        //public List<int> ListOdds(int input)
+        //{
+            /*
+            List<int> listOdds = new List<int>();
+
+
+            for (int i = 0; i < input; i++)
+            {
+                listOdds.Add(i + 2);
+            }
+
+            return listOdds;
+            */
+         //   throw new NotImplementedException();
+       // }
+
+    }
+}
