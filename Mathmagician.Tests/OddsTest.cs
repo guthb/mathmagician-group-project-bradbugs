4b47917c082f70bfb439370b35c97b542bcebdc6
Bradley Guthrie <guppy96@gmail.com>
2016-08-04 21:06:49 -0500

    list test

---
+++ Mathmagician.Tests/OddsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class OddsTest
+    {
+        [TestMethod]
+        //test for class creation
+        public void OddEnsureICanCreateAnInstance()
+        {
+                //Arrange
+
+                //Act
+                Odds my_odd = new Odds();
+
+                //Assert
+                Assert.IsNotNull(my_odd);
+        
+        }
+
+        [TestMethod]
+        //test for list of integers
+        public void OddEnsureICanCreateAList()
+        {
+            //Arrange
+
+            //Act
+            Odds my_odd_list = new Odds();
+
+            //Assert
+            Assert.IsInstanceOfType(my_odd_list.ListOdds(0), typeof(List<int>));
+
+
+
+        }
+
+    }
+}
--- Mathmagician/Odds.cs
+++ Mathmagician/Odds.cs
@@ -9,21 +9,18 @@ namespace Mathmagician
 {
     public class Odds
     {
-        //public List<int> ListOdds(int input)
-        //{
-            /*
+        public List<int> ListOdds(int input)
+        {
+            
             List<int> listOdds = new List<int>();
 
 
-            for (int i = 0; i < input; i++)
-            {
-                listOdds.Add(i + 2);
-            }
+           
 
             return listOdds;
-            */
+            
          //   throw new NotImplementedException();
-       // }
+       }
 
     }
 }
