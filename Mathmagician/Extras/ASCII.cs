b3287f1442bfd96379ef72b7fde610948fb11dcc
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-05 21:11:37 -0500

    start ascii

---
+++ Mathmagician/Extras/ASCII.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician.Extras
+{
+    class ASCII
+    {
+        public string Wizard()
+        {
+            return "              o\r\n"
+                + "                   O       /`-.__\r\n"
+                + "                          / " + @"\" + "·'^|\r\n"
+                + "             o           T    l  *\r\n"
+                + "                        _|-..-|_\r\n"
+                + "                 O    (^ '----' `)\r\n"
+                + "                       `" + @"\" + "-....-/^\r\n"
+                + "             O       o  ) * / * (\r\n"
+                + "                       _( (-)  )_\r\n"
+                + "                   O  /" + @"\" + " )    (  /" + @"\" + "\r\n"
+                + "                     /  " + @"\" + "(    ) |  " + @"\" + "\r\n"
+                + "                 o  o    " + @"\" + ")  ( /    " + @"\" + "\r\n"
+                + "                   /     |(  )|      " + @"\" + "\r\n"
+                + "                  /    o " + @"\" + " " + @"\" + "( /       " + @"\" + "\r\n"
+                + "            __.--'   O    " + @"\" + "_ /   .._   " + @"\" + "\r\n"
+                + "           //|)" + @"\" + "      ,   (_)   /(((" + @"\" + "^)'" + @"\" + "\r\n"
+                + "              |       | O         )  `  |\r\n"
+                + "\r\n";
+        }
+
+    }
+}
